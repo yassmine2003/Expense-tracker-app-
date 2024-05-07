f0da246634fbe6c35fd6403b9b5f4b024b8ee767
karmaaazine <114626534+karmaaazine@users.noreply.github.com>
2024-05-07 22:49:36 +0100

    Update SpendSmartController.java

--- src/controller/SpendSmartController.java
+++ src/controller/SpendSmartController.java
@@ -1,25 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controller;
 
+import java.sql.ResultSet;
+import java.util.Date;
 import model.DataBase;
+import view.Login;
 
 
 public class SpendSmartController {
     private DataBase model;
+    private Login view;
 
     public SpendSmartController() {
         model = new DataBase();
+        view = new Login();
+    }
+    public void initController(){
+        view.setVisible(true);
     }
 
     public boolean authenticateUser(String username, String password) {
         return model.authenticateUser(username, password);
     }
-    public boolean signUpUser(String firstname,String lastname,String username, String password){
-        return model.signUpUser(firstname,lastname,username,password);
+    public boolean signUpUser(String firstname,String lastname,String username,String email, String password) {
+        return model.signUpUser(firstname,lastname,username,email,password);
     }
-    
+    public boolean findPassword(String email){
+        return model.findPassword(email);
+    }
+    public boolean changePassword(String email,String password){
+        return model.changePassword(email,password);
+    }
+    public boolean enterExpense(String username,String expence,String description,String type,Date date){
+        return model.enterExpense(username,expence, description, type, date);
+    }
+    public boolean deleteExpense(String expense, String description, String type, Date date) {
+        return model.deleteExpense(expense, description, type, date);
+    }
+    public boolean modifyExpense(String expense, String description, String type, Date date) {
+        return model.modifyExpense(expense, description, type, date);
+    }
+    public ResultSet fetchData(String username){
+        return model.fetchData(username);
+    }
+    public ResultSet fetchUser(String username){
+        return model.fetchUser(username);
+    }
+    public boolean updateUser(String prenom, String nom,String oldUsername, String newUsername,String email, String password){
+        return model.updateUser(prenom, nom, oldUsername, newUsername, email, password);
+    }
+    public ResultSet fetchReport(String username, int month){
+        return model.fetchReport(username,month);
+    }
+    public boolean updateCharge(String username, double balance, int month, double monthlyBudget){
+        return model.updateCharge(username, balance, month, monthlyBudget);
+    }
+    public boolean enterCharge(String username,double balance, double monthlyBudget, int month){
+        return model.enterCharge(username, balance, monthlyBudget, month);
+    }
 }
